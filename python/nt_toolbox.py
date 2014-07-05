223d672b5fa47b5beddc1939a95e909443328362
Gabriel Peyré <gabriel.peyre@ceremade.dauphine.fr>
2014-07-05 15:19:31 +0200

    added interpolation function.

--- python/nt_toolbox.py
+++ python/nt_toolbox.py
@@ -270,11 +270,31 @@ def gaussian_blur(f, sigma):
 	h = h/np.sum(h)
 	return np.real( pylab.ifft2(pylab.fft2(f) * pylab.fft2(h)) )
 	
-def interp2(x,y,f,xi,yi, k=1):
-	"""
-		interp2(1:n,1:n, G, imag(gamma), real(gamma));
-	"""
-	return ndimage.map_coordinates(f, [x, y], order=k);
+def bilinear_interpolate(im, x, y):
+	    x = np.asarray(x)
+	    y = np.asarray(y)
+
+	    x0 = np.floor(x).astype(int)
+	    x1 = x0 + 1
+	    y0 = np.floor(y).astype(int)
+	    y1 = y0 + 1
+
+	    x0 = np.clip(x0, 0, im.shape[1]-1);
+	    x1 = np.clip(x1, 0, im.shape[1]-1);
+	    y0 = np.clip(y0, 0, im.shape[0]-1);
+	    y1 = np.clip(y1, 0, im.shape[0]-1);
+
+	    Ia = im[ y0, x0 ]
+	    Ib = im[ y1, x0 ]
+	    Ic = im[ y0, x1 ]
+	    Id = im[ y1, x1 ]
+
+	    wa = (x1-x) * (y1-y)
+	    wb = (x1-x) * (y-y0)
+	    wc = (x-x0) * (y1-y)
+	    wd = (x-x0) * (y-y0)
+
+	    return wa*Ia + wb*Ib + wc*Ic + wd*Id
 	
 def grad(f):
 	"""
