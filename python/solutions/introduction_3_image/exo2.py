fd594b645d6118b6e5d7233f68af4c6b22bcac0d
Gabriel Peyré <gabriel.peyre@ceremade.dauphine.fr>
2014-09-14 19:28:28 +0200

    Added python tour on intro to image processing.

--- python/solutions/introduction_3_image/exo2.py
+++ python/solutions/introduction_3_image/exo2.py
@@ -5,5 +5,5 @@ Mf[n/2-k/2+2:n/2+k/2, n/2-k/2+2:n/2+k/2] = 0
 Mh = real( ifft2(Mf) )
 # display
 clf
-imageplot( crop(M), 'Image', 1,2,1)
-imageplot(clamp( crop(Mh)), 'Low pass filtered', 1,2,2)
+imageplot( M[n/2-20:n/2+20,n/2-20:n/2+20], 'Image', [1,2,1])
+imageplot( Mh[n/2-20:n/2+20,n/2-20:n/2+20], 'Low pass filtered', [1,2,2])
