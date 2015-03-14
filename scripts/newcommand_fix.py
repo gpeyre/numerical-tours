edd8318b936f8796a1edd81d2f367b0d3cba2c16
Steven Silvester <steven.silvester@ieee.org>
2015-03-13 22:01:48 -0500

    Allow the newcommand fix to work on a whole directory

--- scripts/newcommand_fix.py
+++ scripts/newcommand_fix.py
@@ -1,18 +1,18 @@
+import glob
 
-fname = "../python/inverse_2_deconvolution_variational.ipynb"
-lines = []
-with open(fname) as fid:
-    for line in fid:
-        if 'newcommand' in line:
-            if line.strip().startswith('"\\\\newcommand'):
-                line = line.replace('"\\\\newcommand', '"$\\\\newcommand')
-            if line.strip().startswith('"\\\\renewcommand'):
-                line = line.replace('"\\\\renewcommand', '"$\\\\newcommand')
-            line = line.replace('\\n"', '$\\n"')
-            print(line)
-        if not line.strip() == '"$"':
-            lines.append(line)
+for fname in glob.glob('../python/todo/*.ipynb'):
+    print(fname)
+    lines = []
+    with open(fname) as fid:
+        for line in fid:
+            if 'newcommand' in line:
+                if line.strip().startswith('"\\\\newcommand'):
+                    line = line.replace('"\\\\newcommand', '"$\\\\newcommand')
+                if line.strip().startswith('"\\\\renewcommand'):
+                    line = line.replace('"\\\\renewcommand', '"$\\\\newcommand')
+                line = line.replace('\\n"', '$\\n"')
+            if not line.strip() == '"$"':
+                lines.append(line)
 
-
-with open(fname, 'w') as fid:
-    fid.writelines(lines)
+    with open(fname, 'w') as fid:
+        fid.writelines(lines)
