d6d1bfc072ceb8960a301bab27084ebe35fddd13
Steven Silvester <steven.silvester@ieee.org>
2015-03-14 21:52:03 -0500

    Update newcommand_fix script

--- scripts/newcommand_fix.py
+++ scripts/newcommand_fix.py
@@ -1,47 +1,32 @@
 import glob
 import json
 
-for fname in glob.glob('../matlab/*.ipynb'):
+for fname in glob.glob('../python/todo/*.ipynb'):
     print(fname)
     lines = []
     with open(fname) as fid:
-        
-        """
-        for line in fid:
-            if 'newcommand' in line: 
-                from PyQt4.QtCore import pyqtRemoveInputHook; pyqtRemoveInputHook()
-                import ipdb; ipdb.set_trace()
-                pass
-                
-                                  
-                line = line.replace('\$\newcommand{', '$\\newcommand{')
-                #line = line.replace('}$"', '}$",')
-            if not line.strip() == '"$"':
-                lines.append(line)
-        """
         try:
             tree = json.load(fid)
         except (ValueError, TypeError) as e:
             print(e)
-            from PyQt4.QtCore import pyqtRemoveInputHook; pyqtRemoveInputHook()
-            import ipdb; ipdb.set_trace()
-            pass
-            
             continue
 
-    for line in tree['worksheets'][0]['cells'][0]['source']:
-        if line.startswith('\\newcommand'):
-            line = '$' + line
-        if line.startswith('\\renewcommand'):
-            line = '$' + line.replace('\\renewcommand', '\\newcommand')
-        line = line.replace('}\n', '}$\n')
-            
-        if not line == '$':
-            lines.append(line)
+    for cell in tree['worksheets'][0]['cells']:
+        lines = []
+        if 'source' in cell:
+            for line in cell['source']:
+                if line.startswith('\\newcommand'):
+                    line = '$' + line
+                if line.startswith('\\renewcommand'):
+                    line = '$' + line.replace('\\renewcommand', '\\newcommand')
+                line = line.replace('}\n', '}$\n')
+                if 'newcommand' in line:
+                    if not line.count('$') == 2:
+                        line += '$'
+                if not line == '$':
+                    lines.append(line)
+            cell['source'] = lines
 
-    tree['worksheets'][0]['cells'][0]['source'] = lines
-    
     with open(fname, 'w') as fid:
-        #fid.writelines(lines)
         print('saving', fname)
         json.dump(tree, fid)
