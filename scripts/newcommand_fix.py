4d9d8d74907364ad2eb58c5f057a21f67fb5fe05
Steven Silvester <steven.silvester@ieee.org>
2015-03-14 21:38:11 -0500

    newcommand fix

--- scripts/newcommand_fix.py
+++ scripts/newcommand_fix.py
@@ -1,18 +1,47 @@
 import glob
+import json
 
 for fname in glob.glob('../matlab/*.ipynb'):
     print(fname)
     lines = []
     with open(fname) as fid:
+        
+        """
         for line in fid:
-            if 'newcommand' in line:
-                if line.strip().startswith('"\\\\newcommand'):
-                    line = line.replace('"\\\\newcommand', '"$\\\\newcommand')
-                if line.strip().startswith('"\\\\renewcommand'):
-                    line = line.replace('"\\\\renewcommand', '"$\\\\newcommand')
-                line = line.replace('}\\n"', '}$\\n"')
+            if 'newcommand' in line: 
+                from PyQt4.QtCore import pyqtRemoveInputHook; pyqtRemoveInputHook()
+                import ipdb; ipdb.set_trace()
+                pass
+                
+                                  
+                line = line.replace('\$\newcommand{', '$\\newcommand{')
+                #line = line.replace('}$"', '}$",')
             if not line.strip() == '"$"':
                 lines.append(line)
+        """
+        try:
+            tree = json.load(fid)
+        except (ValueError, TypeError) as e:
+            print(e)
+            from PyQt4.QtCore import pyqtRemoveInputHook; pyqtRemoveInputHook()
+            import ipdb; ipdb.set_trace()
+            pass
+            
+            continue
 
+    for line in tree['worksheets'][0]['cells'][0]['source']:
+        if line.startswith('\\newcommand'):
+            line = '$' + line
+        if line.startswith('\\renewcommand'):
+            line = '$' + line.replace('\\renewcommand', '\\newcommand')
+        line = line.replace('}\n', '}$\n')
+            
+        if not line == '$':
+            lines.append(line)
+
+    tree['worksheets'][0]['cells'][0]['source'] = lines
+    
     with open(fname, 'w') as fid:
-        fid.writelines(lines)
+        #fid.writelines(lines)
+        print('saving', fname)
+        json.dump(tree, fid)
