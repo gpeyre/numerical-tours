f72043b19a856e0d2dd35a6a440a13ad06c1d457
Lilian Besson <naereen@crans.org>
2016-01-02 22:25:19 +0100

    Update optim_5_primal_dual.py
    Few changes.
    Still has to be modified to be valid a Python program.

--- python/todo/solutions/optim_5_primal_dual.py
+++ python/todo/solutions/optim_5_primal_dual.py
@@ -1,5 +1,5 @@
 def exo1():
-    """
+    r"""
     Implement the primal-dual algorithm.
     Monitor the evolution of the TV energy $F(K(f_k))$
     during the iterations.
@@ -7,27 +7,29 @@ def exo1():
     satisfies the constraints.
     """
     niter = 200
-    E = []; C = []
-    for i in 1: niter:
-        % update
+    E = []
+    C = []
+    for i in range(niter):
+        # Update
         fold = f
         g = ProxFS(g + sigma*K(f1), sigma)
         f = ProxG(f-tau*KS(g), tau)
         f1 = f + theta * (f-fold)
-        % monitor the decay of the energy
-        E(i) = F(K(f))
-        C(i) = snr(f0, f)
-    h = plot(E)
-    set(h, 'LineWidth', 2)
-    axis('tight')
+        # Monitor the decay of the energy
+        E.append(F(K(f)))
+        C.append(snr(f0, f))
+    # FIXME
+    plt.plot(E)
+    plt.axis('tight')
 
 
 def exo2():
-    """
+    r"""
     Use the primal dual scheme to perform regularization in the presence of
     noise
     $$ \umin{\norm{y-\Phi(f)} \leq \epsilon} \norm{\nabla f}_1. $$
     """
+    # TODO
 
 
 def exo3():
@@ -37,23 +39,24 @@ def exo3():
     y = Phi(f0)
     ProxG = lambda f, tau: f + Phi(y - Phi(f))
     niter = 600
-    ndisp = round(linspace(1, niter, 5)); ndisp(1) = []
+    ndisp = np.round(np.linspace(1, niter, 5))
+    ndisp[0] = []
     E = []
     f = y
     g = K(y)*0
     f1 = f
-    q = 1
-    for i in 1: niter:
-        % update
+    q = 0
+    for i in range(niter):
+        # Update
         fold = f
         g = ProxFS(g + sigma*K(f1), sigma)
         f = ProxG(f-tau*KS(g), tau)
         f1 = f + theta * (f-fold)
-        % monitor the decay of the energy
+        # monitor the decay of the energy
         E(i) = F(K(f))
-        if i = =ndisp(q)
-            subplot(2, 2, q)
-            imageplot(f)
+        if i == ndisp[q]:
+            subplot(2, 2, q)  # FIXME
+            imageplot(f)  # FIXME
             q = q + 1
 
 
