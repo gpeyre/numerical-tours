be9e917d534c5dd6f2de36c95ac748b2adc5ef4b
Pierre Stock <pierrestock@users.noreply.github.com>
2016-08-17 22:19:12 +0200

    Update plot_mesh.py

--- python/nt_toolbox/plot_mesh.py
+++ python/nt_toolbox/plot_mesh.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from skimage import measure
 from mpl_toolkits.mplot3d import Axes3D
     
-def plot_mesh(X, F, subplot = [1,1,1], el=90, az=-90, title=""):
+def plot_mesh(X, F, subplot = [1,1,1], title="", el=90, az=-90, lwdt=.1, dist=6):
     """
     plots the mesh defined by X : vertices and F : faces
     """
     ax = plt.subplot(subplot[0],subplot[1],subplot[2], projection='3d')
-    ax.plot_trisurf(X[0,:], X[1,:], X[2,:], triangles=np.transpose(F), lw=0.1, color="grey", alpha=1)
+    ax.plot_trisurf(X[0,:], X[1,:], X[2,:], triangles=np.transpose(F), lw=lwdt, color="grey", alpha=1)
     ax.axis("off")
     ax.view_init(elev=el, azim=az)
-    ax.dist = 6
+    ax.dist = dist
     plt.title(title)
