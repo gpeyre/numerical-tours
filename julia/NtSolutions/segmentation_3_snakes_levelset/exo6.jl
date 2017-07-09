753b130673396c4cee40b2451b0eff3eb98a804f
Ayman <ayman.chaouki@student.ecp.fr>
2017-07-09 18:50:44 +0200

    Making isosurface.jl and perform_redistancing.jl independant from the package

--- julia/NtSolutions/segmentation_3_snakes_levelset/exo6.jl
+++ julia/NtSolutions/segmentation_3_snakes_levelset/exo6.jl
@@ -1,17 +1,18 @@
 figure(figsize = (10, 10))
 phi = copy(phi0)
 k = 0
+gW = NtToolBox.Grad(W, "sym", 2)
 
 for i in 1 : niter
-    gD = NtToolBox.Grad(phi, "sym",2)
-    d = max(eps().*ones(n, n), sqrt(sum(gD.^2, 3)))
+    gD = NtToolBox.Grad(phi, "sym", 2)
+    d = max(eps().*ones(n, n), sqrt(sum(g0.^2, 3)))
     g = gD./cat(3, d[:, :], d[:, :])
-    G = d.*NtToolBox.Div(g[:, :, 1], g[:, :, 2], "sym", 2) - lambd*(f0 - c1).^2 + lambd*(f0 - c2).^2
+    G = W.*(d.*NtToolBox.Div(g[:, :, 1], g[:, :, 2], "sym", 2)) + sum(gW.*gD, 3)
     phi = (phi + tau.*G)[:, :]
     if i % 30 == 0
         phi = perform_redistancing(phi)
     end
-    if i % Base.div(niter, 4) == 0
+    if i % Base.div(niter, 4.) == 0
         k = k + 1
         subplot(2, 2, k)
         NtToolBox.plot_levelset(phi, 0, f0)
