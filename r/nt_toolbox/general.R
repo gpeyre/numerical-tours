f0bc2a4f9679e3f66ca42c5337e126803f67c85a
Alex_D <delalande.alex@gmail.com>
2017-11-08 19:39:37 +0100

    finished denoisingwav_2_wavelet_2d tutorial

--- r/nt_toolbox/general.R
+++ r/nt_toolbox/general.R
@@ -66,6 +66,31 @@ roll <- function(x, n, axis=1){
 
 
 
+circshift <- function(x, p){
+  ####
+  # Circular shift of an array.
+  ####
+  y <- as.matrix(x)
+  if (p[1] > 1){
+    y <- rbind( y[p[1]:dim(y)[1],,drop=F], y[1:(p[1]-1),,drop=F] )
+    if (dim(x)[2]>0 & length(p)>1){
+      y <- cbind( y[, p[1]:dim(y)[2],drop=F], y[, 1:(p[1]-1),drop=F] )
+    }
+  }
+  else if (p[1] < -1){
+    p[1] <- dim(y)[1] + p[1] + 2
+    y <- rbind( y[p[1]:dim(y)[1],,drop=F], y[1:(p[1]-1),,drop=F] )
+    if (dim(x)[2]>0 & length(p)>1){
+      y <- cbind( y[, p[1]:dim(y)[2],drop=F], y[, 1:(p[1]-1),drop=F] )
+    }
+  }
+  return(y)
+}
+
+
+
+
+
 circshift1d <- function(x, k){
   ####
   # Circularly shift a 1D vector
