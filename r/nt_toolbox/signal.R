f295a665ec62f0f1b92f4830be1371802feae0ca
abdel lam <abdellam1994@gmail.com>
2017-12-27 16:06:49 +0000

    Finished first two ml tutorials

--- r/nt_toolbox/signal.R
+++ r/nt_toolbox/signal.R
@@ -375,6 +375,17 @@ ifftshift <- function(input_matrix, dim = -1) {
 }
 
 
+diag = function(X)
+{
+    """ Extract the diagonal of a matrix and returns a vector """
+    out = rep(0, dim(X)[1])
+    for (i in 1:length(out))
+    {
+    out[i] = X[i,i]
+    }
+    return(out)
+    
+}
 
 
 
