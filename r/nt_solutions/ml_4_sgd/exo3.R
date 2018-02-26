6cd22e670502f499072050be3494af66593dbe8d
abdel lam <abdellam1994@gmail.com>
2018-02-26 09:53:06 +0000

    Corrected the SGD NT

--- r/nt_solutions/ml_4_sgd/exo3.R
+++ r/nt_solutions/ml_4_sgd/exo3.R
@@ -16,24 +16,26 @@ for (is in 1:nsamples)
         {
             ElistA[1 + floor((it-1)/err_rate), is] = E(w,X,y)
         }
-        tau = tau0 / (1 + sqrt(it/ell0))
+        tau = tau0 / (1 + sqrt((it-1)/ell0))
         i = (1 + floor(rand() * n))[1] # draw uniformly
-        w1 = w1 - tau * nablaEi(w,i)
+        w1 = w1 - tau * nablaEi(w1,i)
         w = (1/it) * w1 + (1 - (1/it)) * w
     }
 }
 
+vmin = min(min(ElistS), min(ElistA), min(Elist))
+
 # Getting the boundaries for the plot
-min_1 = min(log10(ElistS-min(Elist)))
-min_2 = min(log10(ElistA-min(Elist)))
-max_1 = max(log10(ElistS-min(Elist)))
-max_2 = max(log10(ElistA-min(Elist)))
+min_1 = min(log10(ElistS-vmin))
+min_2 = min(log10(ElistA-vmin))
+max_1 = max(log10(ElistS-vmin))
+max_2 = max(log10(ElistA-vmin))
 min_plot = min(min_1, min_2)
 max_plot = max(max_1, max_2)
 
-matplot(seq(1, niter, err_rate), log10(ElistS-min(Elist)), type="l", col=4, lwd=2, 
+matplot(seq(1, niter, err_rate), log10(ElistS-vmin+1e-18), type="l", col=4, lwd=2, 
         xlab="", ylab="", main="log(E(w_l) - min E)", ylim=c(min_plot, max_plot))
 par(new=TRUE)
-matplot(seq(1, niter, err_rate), log10(ElistA-min(Elist)), type="l", col=2,
+matplot(seq(1, niter, err_rate), log10(ElistA-vmin + 1e-18), type="l", col=2,
         lwd=2, xlab="", ylab="", ylim=c(min_plot, max_plot))
 legend("topright", legend=c("SGD", "SGA"), col=c(4,2), pch="-")
