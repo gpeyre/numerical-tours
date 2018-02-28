a40c3bef678b401abba8d7b264a198ae8135d696
abdel lam <abdellam1994@gmail.com>
2018-02-28 21:58:37 +0000

    modified exercise 4 meshproc3

--- r/nt_solutions/meshproc_3_denoising/exo4.R
+++ r/nt_solutions/meshproc_3_denoising/exo4.R
@@ -1,15 +1,16 @@
-ntests <- 15
-muList <- seq(from=3, to = 15, by=(15-3)/(ntests-1))/5
-errR <- c()
+max_iter = 50
+ntests = 5
+muList = seq(from=3, to = 15, by=(15-3)/(ntests-1))/5
+errR = c()
 
 for (i in (1:ntests)){
-  mu <- muList[i]
-  A <- .sparseDiagonal(x=1, n) + mu*L
+  mu = muList[i]
+  A = .sparseDiagonal(x=1, n) + mu*L
   
   for (k in (1:3)){
-    Xmu[k,] <- t(as.matrix(pcg(as.matrix(X[k,]), max_iter)))
+    Xmu[k,] = t(as.matrix(pcg(as.matrix(X[k,]), max_iter)))
     
-  errR <- c(errR,snr(X0,Xmu))
+  errR = c(errR,snr(X0,Xmu))
   }
 }
 
