14ef9bfc5c2c879b443a34b2ef4eda71134be44f
Gabriel Peyré <gabriel.peyre@ens.fr>
2018-05-02 18:08:43 +0200

    removed lena test image

--- python/nt_solutions/graphics_5_fluids/exo3.py
+++ python/nt_solutions/graphics_5_fluids/exo3.py
@@ -1,20 +1,28 @@
 plt.figure(figsize = (10,10))
-V = normalize(ProjI(V))
+U = ProjI(V)
 g = np.copy(f)
 k=0
 
+tau = .03
+niter = 30
+
 for i in range(1,niter+1):
     # advect
-    g = W(g,tau*U)
-    V = Wt(V,tau*U)
+    g = W(g,tau*V)
+    U = Wt(U,tau*V)
+
+    #Z = U
+    #U[:,:,0] = W(Z[:,:,0],tau*Z)
+    #U[:,:,1] = W(Z[:,:,1],tau*Z)
+
     # diffuse
-    V = V + tau*nu*Delta(V)
+    U = U + tau*nu*DeltaV(U)
     g = g + tau*mu*Delta(g)
     # project
-    V = ProjI(V)
+    U = ProjI(U)
     # additional constraints
-    
+
     #display
     if i%(niter//4) == 0:
         k +=1
-        imageplot(g, "Time = %i" %(i*tau), [2,2,k])
+        imageplot(g, "Time = %f" %(i*tau), [2,2,k])
--- python/nt_solutions/introduction_3_image/exo2.py
+++ python/nt_solutions/introduction_3_image/exo2.py
@@ -1,9 +1,10 @@
 k = round(.8*n)
 k = round(k/2)*2 # even number
 Mf = fft2(M)
-Mf[n/2-k/2+2:n/2+k/2, n/2-k/2+2:n/2+k/2] = 0
+r = int(k/2)
+Mf[m-r+2:m+r, m-r+2:m+r] = 0
 Mh = real( ifft2(Mf) )
 # display
 clf
-imageplot( M[n/2-20:n/2+20,n/2-20:n/2+20], 'Image', [1,2,1])
-imageplot( Mh[n/2-20:n/2+20,n/2-20:n/2+20], 'Low pass filtered', [1,2,2])
+imageplot( M[m-20:m+20,m-20:m+20], 'Image', [1,2,1])
+imageplot( Mh[m-20:m+20,m-20:m+20], 'Low pass filtered', [1,2,2])
