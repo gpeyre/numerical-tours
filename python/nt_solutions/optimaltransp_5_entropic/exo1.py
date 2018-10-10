68c6ff9ee45ece5e18b5d192e7d3648f4e14cd29
Gabriel Peyré <gabriel.peyre@ens.fr>
2018-10-10 19:07:13 +0200

    update zip

--- python/nt_solutions/optimaltransp_5_entropic/exo1.py
+++ python/nt_solutions/optimaltransp_5_entropic/exo1.py
@@ -1,24 +1,32 @@
 from numpy import linalg
 
+gamma = .01;
+xi = np.exp(-C/gamma)
+
 b = np.ones(N[1])
-niter = 300
+niter = 1000
 Err_p = []
 Err_q = []
 
 for i in range(niter):
+    # sinkhorn step 1
     a = p/(np.dot(xi,b))
-    Err_q = Err_q + [linalg.norm(b*np.dot(np.transpose(xi),a) - q)/linalg.norm(q)]
+    # error computation
+    r = b*np.dot(np.transpose(xi),a)
+    Err_q = Err_q + [linalg.norm(r - q, 1)]
+    # sinkhorn step 2
     b = q /(np.dot(np.transpose(xi),a))
-    Err_p = Err_p + [linalg.norm(a*np.dot(xi,b) - p)/linalg.norm(p)]
+    s = a*np.dot(xi,b)
+    Err_p = Err_p + [linalg.norm(s - p,1)]
 
 plt.figure(figsize = (10,7))
 
 plt.subplot(2,1,1)
-plt.title("$||\pi -p||$")
+plt.title("$||\pi 1 -p||$")
 plt.plot(np.log(np.asarray(Err_p) + 1e-5), linewidth = 2)
 
 plt.subplot(2,1,2)
-plt.title("$||\pi^T -q||$")
+plt.title("$||\pi^T 1 -q||$")
 plt.plot(np.log(np.asarray(Err_q) + 1e-5), linewidth = 2)
 
-plt.show()  
+plt.show()
--- python/nt_solutions/optimaltransp_5_entropic/exo3.py
+++ python/nt_solutions/optimaltransp_5_entropic/exo3.py
@@ -6,10 +6,15 @@ Err_p = []
 Err_q = []
 
 for i in range(niter):
+    # sinkhorn step 1
     a = p/(np.dot(xi,b))
-    Err_q = Err_q + [linalg.norm(b*np.dot(xi,a) - q)/linalg.norm(q)]
+    # error computation
+    r = b*np.dot(np.transpose(xi),a)
+    Err_q = Err_q + [linalg.norm(r - q, 1)]
+    # sinkhorn step 2
     b = q /(np.dot(np.transpose(xi),a))
-    Err_p = Err_p + [linalg.norm(a*np.dot(np.transpose(xi),b) - p)/linalg.norm(p)]
+    s = a*np.dot(xi,b)
+    Err_p = Err_p + [linalg.norm(s - p,1)]
 
 plt.figure(figsize = (10,7))
 
@@ -21,4 +26,4 @@ plt.subplot(2,1,2)
 plt.title("$||\pi^T -q||$")
 plt.plot(np.log(np.asarray(Err_q)), linewidth = 2)
 
-plt.show()                                 
+plt.show()
