375e6c9963a71b8bfb76cbc4771151e2556308b5
Gabriel Peyré <gabriel.peyre@ens.fr>
2019-02-07 09:41:10 +0100

    added MLP

--- python/nt_solutions/optim_1_gradient_descent/exo1.py
+++ python/nt_solutions/optim_1_gradient_descent/exo1.py
@@ -3,11 +3,11 @@ niter = 20
 E = np.zeros((niter,1))
 X = np.zeros((2,niter))
 for i in np.arange(0,niter):
-    X[:,i] = x.flatten()
+    X[:,i] = x
     E[i] = f(x)
     x = x - tau*Gradf(x)
 
 plt.clf
 h = plt.plot(np.log10(E))
 plt.axis('tight')
-plt.title('log_{10}(x^{(k)})')
+plt.title('$log_{10}f(x^{(k)})$')
